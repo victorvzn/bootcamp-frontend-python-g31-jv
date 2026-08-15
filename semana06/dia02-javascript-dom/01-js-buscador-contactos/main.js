9e56657c0a1908925e1f71033f3630a37146990f
Victor Villazón <victorzn23@gmail.com>
2026-08-14 20:10:09 -0500

    semana06, dia01, 01, javascript dom buscador de contactos

---
+++ semana06/dia02-javascript-dom/01-js-buscador-contactos/main.js
@@ -0,0 +1,22 @@
+const searchInput = document.querySelector('#search-input')
+const contactList = document.querySelector('#contact-list')
+const contacts = document.querySelectorAll('.contact')
+
+// console.log(searchInput)
+// console.log(contactList)
+
+// searchInput.addEventListener(EVENTO, FUNCION)
+searchInput.addEventListener('input', function(event) {
+  const query = event.target.value.toLowerCase()
+
+  contacts.forEach(function(contact) {
+    const name = contact.querySelector('.name').textContent.toLowerCase()
+    const coincidencias = name.includes(query)
+
+    if (coincidencias) {
+      contact.classList.remove('hidden')
+    } else {
+      contact.classList.add('hidden')
+    }
+  })
+})
