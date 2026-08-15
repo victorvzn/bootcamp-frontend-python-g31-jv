07e79ed5f3c1043dc0bb3906113df8572af65594
Victor Villazón <victorzn23@gmail.com>
2026-08-14 21:55:17 -0500

    semana06, dia01, 01, javascript dom tareas app

---
+++ semana06/dia02-javascript-dom/02-js-tareas-app/main.js
@@ -0,0 +1,25 @@
+const taskInput = document.getElementById('task-input')
+// const taskInput = document.querySelector('#task-input')
+const taskAdd = document.querySelector('.task__add')
+const taskList = document.querySelector('.task__list')
+
+// EVENTOS
+// ELEMENTO.addEventListener(NOMBRE_EVENTO, CALLBACK)
+
+taskAdd.addEventListener('click', function(event) {
+  // Se ejecutará cuando hagamos click en el botón 'Añadir tarea'
+  console.log('Click!!!', taskInput.value)
+
+  // TODO: 01 - Añadir el elemento li al elemento con la clase task__list
+  const li = document.createElement('li')
+
+  // TODO: 02 - Añadir el elemento span al elemento li con el texto de la caja taskInput
+
+  taskList.appendChild(li)
+
+  // MANEJO DEL DOM: Forma de crear elementos dinámicamente con el DOM de Javascript
+  // const button = document.createElement('button')
+  // console.log({ button })
+  // button.textContent = 'Hola soy un botón'
+  // document.body.appendChild(button)
+})
