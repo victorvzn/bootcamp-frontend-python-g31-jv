a306214735478b839a919698b3da464005484b3a
Victor Villazón <victorzn23@gmail.com>
2026-08-14 20:43:14 -0500

    semana06, dia01, 01, javascript dom buscador de contactos

--- semana06/dia02-javascript-dom/01-js-buscador-contactos/main.js
+++ semana06/dia02-javascript-dom/01-js-buscador-contactos/main.js
@@ -9,9 +9,12 @@ const contacts = document.querySelectorAll('.contact')
 searchInput.addEventListener('input', function(event) {
   const query = event.target.value.toLowerCase()
 
+  // TODO: Tambien debe fitlrar por el correo del contacto
+
   contacts.forEach(function(contact) {
     const name = contact.querySelector('.name').textContent.toLowerCase()
-    const coincidencias = name.includes(query)
+    const email = contact.querySelector('.email').textContent.toLowerCase()
+    const coincidencias = name.includes(query) || email.includes(query)
 
     if (coincidencias) {
       contact.classList.remove('hidden')
