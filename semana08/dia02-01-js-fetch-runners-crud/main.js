694e0acfe6022a0a8957458a514f9a6019ee0961
Victor Villazón <victorzn23@gmail.com>
2026-08-28 20:44:53 -0500

    semana08, dia01, 02, javascript fetch async await

--- semana08/dia02-01-js-fetch-runners-crud/main.js
+++ semana08/dia02-01-js-fetch-runners-crud/main.js
@@ -1,1 +1,15 @@
-console.log('Hola js')
+const API_URL = 'https://apibox.vercel.app/2aSCD2Sbz4kg4AcacfeJLMmalhKR3Xgs/api/corredores'
+
+const cargarCorredores = async () => {
+  const respuesta = await fetch(API_URL)
+
+  const data = await respuesta.json()
+
+  renderCorredores(data)
+}
+
+const renderCorredores = (corredores = []) => {
+  // TODO: Mostrar los corredores usando la plantilla del li en el index.html
+}
+
+cargarCorredores()
