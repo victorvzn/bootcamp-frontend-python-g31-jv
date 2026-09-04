7e512fbb82513b6166f34fbc37a79d2f03535dd8
Victor Villazón <victorzn23@gmail.com>
2026-09-03 22:12:13 -0500

    semana09, dia01, 01, react introducción

--- semana09/dia01-01-react-introduccion/src/App.jsx
+++ semana09/dia01-01-react-introduccion/src/App.jsx
@@ -1,5 +1,25 @@
+// 01 - Componente base usando function
+
 function App() {
   return <h1>Hola React.js</h1>
 }
 
+// const App = () => {
+//   return <h1>Hola React.js</h1>
+// }
+
 export default App
+
+// ¿Qué es un componente?
+// * Es una función, devuelve JSX, todo componente en su nombre empieza con mayúscula
+
+// Partes de un componente:
+// 1. Imports (Si los hay)
+// 2. Lógica (variables, hooks, functions)
+// 3. Return -> JSX
+// 4. Export (Nos sirve para crear un módulo de ES)
+
+// Reglas básicas de un componente:
+// * Un solo elemente padre
+// * Es recomendable que la función tenga el mismo nombre del archivo. Ej. App.jsx -> App
+
