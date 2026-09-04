381a36c0e790e2da64735a8d40f3a6ac4d2acbff
Victor Villazón <victorzn23@gmail.com>
2026-09-03 21:46:55 -0500

    semana09, dia01, 01, react introducción

---
+++ semana09/dia01-01-react-introduccion/src/modulo.js
@@ -0,0 +1,13 @@
+// Exportación nombrada
+export const frutas = ['manzanas', 'plátanos', 'peras']
+
+// Exportación por defecto
+const nombre = 'Victor'
+
+export default nombre
+
+// Exportación nombrada
+export const curso = {
+  nombre: 'Algoritmos',
+  nota: 20
+}
---
+++ semana09/dia01-01-react-introduccion/src/modulos-esm.js
@@ -0,0 +1,9 @@
+// ESM (ECMAScript Modules)
+// Es una forma moderna de importar archivos, exportar archivos, compartir código entre archivos.
+// Gracias a esto podemos dividir el proyecto en partes pequeñas y organizadas.
+
+import miNombre, { frutas, curso } from "./modulo";
+
+console.log(frutas)
+console.log(miNombre)
+console.log(curso)
