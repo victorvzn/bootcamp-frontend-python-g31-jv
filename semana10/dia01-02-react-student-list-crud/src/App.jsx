6594034fe420476f5ab37bd3093d3951940bea8e
Victor Villazón <victorzn23@gmail.com>
2026-09-10 22:53:37 -0500

    semana10, dia01, 02, react student list crud

--- semana10/dia01-02-react-student-list-crud/src/App.jsx
+++ semana10/dia01-02-react-student-list-crud/src/App.jsx
@@ -1,6 +1,60 @@
 const App = () => {
   return (
-    <div className="font-bold text-amber-600 text-2xl">App</div>
+    <main>
+      <h1>Student CRUD</h1>
+
+      <form>
+        <label>
+          <span>Name</span>
+          <input
+            type="text"
+            name="name"
+            placeholder="Ex. Victor Villazón"
+            required
+          />
+        </label>
+
+        <label>
+          <span>City</span>
+          <input
+            type="text"
+            name="city"
+            placeholder="Ex. Chiclayo"
+            required
+          />
+        </label>
+
+        <div>
+          <input
+            type="submit"
+            value="Save"
+          />
+          <input
+            type="reset"
+            value="Clear"
+          />
+        </div>
+      </form>
+
+      <h2>Student list</h2>
+
+      <section>
+        <div>
+          <div>Name</div>
+          <div>city</div>
+          <div>Actions</div>
+        </div>
+
+        <div>
+          <div>Student 1</div>
+          <div>Chiclayo</div>
+          <div>
+             <button>✏</button>
+             <button>❌</button>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
