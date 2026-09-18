5bd1d82fa97f332a718a2c1e50e0fcd606c381bc
Victor Villazón <victorzn23@gmail.com>
2026-09-17 19:52:01 -0500

    semana11, dia01, 01, react useeffect hooks

--- semana11/dia01-01-react-useeffect-hooks/src/App.jsx
+++ semana11/dia01-01-react-useeffect-hooks/src/App.jsx
@@ -1,5 +1,35 @@
+// useEffect: Sirve para decirle a React que después de mostrar o actualizar un componente, quiero hacer algo.
+
+import { useEffect, useState } from "react"
+
 export default function App() {
+  const [count, setCount] = useState(0)
+
+  // console.log('Hola a todos')
+
+  useEffect(() => {
+    // Se ejecuta en cada render
+    // console.log('El componente apareció')
+    console.log('El contador cambió:', count)
+  })
+
+  useEffect(() => {
+    // Solo se ejecuta al aparecer el componente por primera vez
+    console.log('Imprime esto solamente cuando el componente aparezca por primera vez')
+  }, [])
+
+  useEffect(() => {
+    console.log('Cuando cambia count')
+  }, [count])
+
   return (
-    <div className="text-2xl">App</div>
+    <div className="text-2xl">
+
+      <h1>Count: {count}</h1>
+
+      <button onClick={() => setCount(count + 1)}>+1</button>
+
+    </div>
+    
   )
 }
