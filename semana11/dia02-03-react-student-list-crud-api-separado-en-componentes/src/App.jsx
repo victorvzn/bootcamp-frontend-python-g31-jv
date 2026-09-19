a0a959f828b5df1cafaeb352d1bce6d0235f00a4
Victor Villazón <victorzn23@gmail.com>
2026-09-18 20:07:55 -0500

    semana11, dia02, 03, react student list crud api separado en componentes

--- semana11/dia02-03-react-student-list-crud-api-separado-en-componentes/src/App.jsx
+++ semana11/dia02-03-react-student-list-crud-api-separado-en-componentes/src/App.jsx
@@ -2,76 +2,16 @@ import { useEffect, useState } from "react"
 
 import Swal from 'sweetalert2'
 import { createStudent, fetchStudents, removeStudent, updateStudent } from "./services/students"
+import Form from "./components/Form"
 
 const App = () => {
   const [students, setStudents] = useState([])
-  const [form, setForm] = useState({
-    id: '',
-    name: '',
-    city: ''
-  })
 
   useEffect(() => {
     console.log('Cargando students...')
     fetchStudents()
       .then(data => setStudents(data))
   }, []) // Este useEffect se ejecuta la primera vez que el componente se crea
-
-  const handleSave = (event) => {
-    event.preventDefault()
-
-    console.log('Guardando...')
-
-    // TODO: Implementar el guardado del estudiante cuando este ya existe
-
-    if (form.id) { // Actualizar un estudiante
-      const student = {
-        name: form.name,
-        city: form.city,
-      }
-
-      updateStudent(student, form.id)
-        .then(() => {
-          fetchStudents()
-            .then(data => setStudents(data))
-        })
-
-      setForm({
-        id: '',
-        name: '',
-        city: ''
-      })
-
-      return
-    }
-    
-    // Creando un nuevo estudiante
-
-    // TODO: Crear un registro en el recurso students y posteriormente actualizar el listado de estudiantes
-
-    const newStudent = {
-      name: form.name,
-      city: form.city
-    }
-
-    createStudent(newStudent)
-      .then(() => {
-        fetchStudents()
-          .then(data => setStudents(data))
-      })
-    
-    setForm({
-      id: '',
-      name: '',
-      city: ''
-    })
-  }
-
-  const handleChange = (event) => {
-    const { name, value } = event.target
-
-    setForm({ ...form, [name]: value })
-  }
 
   const handleDelete = (id) => {
     // TODO: Implementar el botón eliminar de cada estudiante para eliminarlo en el apibox
@@ -108,62 +48,11 @@ const App = () => {
     })
   }
 
-  const handleClear = () => {
-    setForm({
-      id: '',
-      name: '',
-      city: ''
-    })
-  }
-
   return (
     <main className="w-96 mx-auto border border-slate-400 rounded-lg mt-6 p-4">
       <h1 className="text-2xl text-center text-slate-700 font-bold mb-4">Student CRUD</h1>
 
-      <form
-        className="flex flex-col gap-4 bg-slate-100 p-3 rounded-lg border"
-        onSubmit={handleSave}
-      >
-        <label className="flex flex-col gap-2">
-          <span className="text-sm font-medium text-slate-900">Name</span>
-          <input
-            className="bg-slate-50 border border-slate-300 text-slate-90 text-sm rounded-lg w-full px-4 py-2"
-            type="text"
-            name="name"
-            placeholder="Ex. Victor Villazón"
-            required
-            onChange={handleChange}
-            value={form.name}
-          />
-        </label>
-
-        <label className="flex flex-col gap-2">
-          <span className="text-sm font-medium text-slate-900">City</span>
-          <input
-            className="bg-slate-50 border border-slate-300 text-slate-90 text-sm rounded-lg w-full px-4 py-2"
-            type="text"
-            name="city"
-            placeholder="Ex. Chiclayo"
-            required
-            onChange={handleChange}
-            value={form.city}
-          />
-        </label>
-
-        <div className="flex gap-4">
-          <input
-            className="bg-blue-700 text-white hover:bg-blue-800 font-medium rounded-lg text-sm w-full px-4 py-2 text-center cursor-pointer"
-            type="submit"
-            value="Save"
-          />
-          <input
-            className="bg-slate-500 text-white hover:bg-slate-600 font-medium rounded-lg text-sm w-full px-4 py-2 text-center cursor-pointer"
-            type="button"
-            value="Clear"
-            onClick={handleClear}
-          />
-        </div>
-      </form>
+      <Form />
 
       <h2 className="text-center text-slate-700 font-bold my-4">Student list</h2>
 
@@ -187,7 +76,6 @@ const App = () => {
           )
         })}
 
-        <pre>{JSON.stringify(form, null, 2)}</pre>
         <pre>{JSON.stringify(students, null, 2)}</pre>
       </section>
     </main>
---
+++ semana11/dia02-03-react-student-list-crud-api-separado-en-componentes/src/components/Form.jsx
@@ -0,0 +1,124 @@
+import { useState } from "react"
+
+const Form = () => {
+  const [form, setForm] = useState({
+    id: '',
+    name: '',
+    city: ''
+  })
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleClear = () => {
+    setForm({
+      id: '',
+      name: '',
+      city: ''
+    })
+  }
+
+  const handleSave = (event) => {
+    event.preventDefault()
+
+    console.log('Guardando...')
+
+    // TODO: Implementar el guardado del estudiante cuando este ya existe
+
+    if (form.id) { // Actualizar un estudiante
+      const student = {
+        name: form.name,
+        city: form.city,
+      }
+
+      updateStudent(student, form.id)
+        .then(() => {
+          fetchStudents()
+            .then(data => setStudents(data))
+        })
+
+      setForm({
+        id: '',
+        name: '',
+        city: ''
+      })
+
+      return
+    }
+    
+    // Creando un nuevo estudiante
+
+    // TODO: Crear un registro en el recurso students y posteriormente actualizar el listado de estudiantes
+
+    const newStudent = {
+      name: form.name,
+      city: form.city
+    }
+
+    createStudent(newStudent)
+      .then(() => {
+        fetchStudents()
+          .then(data => setStudents(data))
+      })
+    
+    setForm({
+      id: '',
+      name: '',
+      city: ''
+    })
+  }
+
+  return (
+    <form
+      className="flex flex-col gap-4 bg-slate-100 p-3 rounded-lg border"
+      onSubmit={handleSave}
+    >
+      <label className="flex flex-col gap-2">
+        <span className="text-sm font-medium text-slate-900">Name</span>
+        <input
+          className="bg-slate-50 border border-slate-300 text-slate-90 text-sm rounded-lg w-full px-4 py-2"
+          type="text"
+          name="name"
+          placeholder="Ex. Victor Villazón"
+          required
+          onChange={handleChange}
+          value={form.name}
+        />
+      </label>
+
+      <label className="flex flex-col gap-2">
+        <span className="text-sm font-medium text-slate-900">City</span>
+        <input
+          className="bg-slate-50 border border-slate-300 text-slate-90 text-sm rounded-lg w-full px-4 py-2"
+          type="text"
+          name="city"
+          placeholder="Ex. Chiclayo"
+          required
+          onChange={handleChange}
+          value={form.city}
+        />
+      </label>
+
+      <div className="flex gap-4">
+        <input
+          className="bg-blue-700 text-white hover:bg-blue-800 font-medium rounded-lg text-sm w-full px-4 py-2 text-center cursor-pointer"
+          type="submit"
+          value="Save"
+        />
+        <input
+          className="bg-slate-500 text-white hover:bg-slate-600 font-medium rounded-lg text-sm w-full px-4 py-2 text-center cursor-pointer"
+          type="button"
+          value="Clear"
+          onClick={handleClear}
+        />
+      </div>
+
+      <pre>{JSON.stringify(form, null, 2)}</pre>
+    </form>
+  )
+}
+
+export default Form
