d57d9d44733e0e20d96f714d0ffccb39acf00700
Victor Villazón <victorzn23@gmail.com>
2026-09-24 19:36:51 -0500

    semana12, dia01, 01, react custom hooks y ejemplos

--- semana12/dia01-01-react-custom-hooks-ejemplos/src/App.jsx
+++ semana12/dia01-01-react-custom-hooks-ejemplos/src/App.jsx
@@ -1,9 +1,19 @@
+import Counter from "./components/Counter"
+
 const App = () => {
   return (
-    <main>
-      <h1>Custom hooks</h1>
+    <main className="m-4">
+      <h1 className="text-2xl text-center py-4">Custom hooks</h1>
 
-      
+      <p className="">Es una función de Javascript que reutiliza lógica de React entre diferentes componentes.</p>
+
+      <ul>
+        <li className="list-disc">No es un componente ❌</li>
+        <li className="list-disc">No devuelve JSX necesariamente</li>
+        <li className="list-disc">Es un función que puede utilizar otros hooks (useState, useEffect, customHooks, etc).</li>
+      </ul>
+
+      <Counter />
 
     </main>
   )
