d6cd4b22e104b27038e7a77abb7af01a42d9572a
Victor Villazón <victorzn23@gmail.com>
2026-09-24 19:53:43 -0500

    semana12, dia01, 01, react custom hooks y ejemplos

--- semana12/dia01-01-react-custom-hooks-ejemplos/src/components/Counter.jsx
+++ semana12/dia01-01-react-custom-hooks-ejemplos/src/components/Counter.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react"
 
 const Counter = () => {
-  const [contador, setContador] = useState(0)
+  const [contador, setContador] = useState(99)
+
+  const decrement = () => setContador(contador - 1)
+
+  const increment = () => setContador(contador + 1)
 
   return (
     <div className="bg-slate-200 p-4 border">
       <h3 className="text-2xl">Contador</h3>
 
       <div className="flex gap-2">
-        <button className="bg-blue-400 px-3 py-2">-</button>
-        <strong className="text-2xl">0</strong>
-        <button className="bg-blue-400 px-3 py-2">+</button>
+        <button className="bg-blue-400 px-3 py-2" onClick={decrement}>-</button>
+        <strong className="text-2xl">{contador}</strong>
+        <button className="bg-blue-400 px-3 py-2" onClick={increment}>+</button>
       </div>
     </div>
   )
