26689332f3bca730abf76b378e4216086d2fe72c
Victor Villazón <victorzn23@gmail.com>
2026-09-25 20:12:58 -0500

    semana12, dia01, 02, react router declarative mode tailwindcss

--- semana12/dia01-02-react-router-declarative-mode-tailwindcss/src/layouts/LayoutProducts.jsx
+++ semana12/dia01-02-react-router-declarative-mode-tailwindcss/src/layouts/LayoutProducts.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router'
 
 const LayoutProducts = () => {
   // TODO: Añadir el header en color azúl para este layout y que muestre la lista de productos solo para /products y /products/:id
   return (
-    <div>LayoutProducts</div>
+    <>
+      <header className="bg-blue-300 p-4">
+        <div className="container mx-auto flex justify-between">
+          <h1 className="font-bold">React router</h1>  
+
+          <nav className="flex gap-4 font-medium">
+            <Link to='/'>Home</Link>
+            <Link to='/prueba'>Prueba</Link>
+            <Link to='/products'>Products</Link>
+          </nav>
+        </div>   
+      </header>
+
+      <main className="container mx-auto m-6">
+        <Outlet />
+      </main>
+    </>
   )
 }
 
