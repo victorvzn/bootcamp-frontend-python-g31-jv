a103b8e37584bc8aafaf1ddb7cf2d854775706a0
Victor Villazón <victorzn23@gmail.com>
2026-09-25 21:42:59 -0500

    semana12, dia02, 03, react zustand counter shopping cart app tailwindcss

--- semana12/dia02-03-react-zustand-counter-shopping-cart-app/src/App.jsx
+++ semana12/dia02-03-react-zustand-counter-shopping-cart-app/src/App.jsx
@@ -1,11 +1,24 @@
 import Counter from "./components/Counter"
 
+import ProductList from "./components/ProductList"
+import ShoppingCart from "./components/ShoppingCart"
+
 const App = () => {
   return (
-    <div className="text-2xl">
-      
-      <Counter />
+    <div className="flex flex-col gap-8 p-4">
 
+      <section className="flex gap-2">
+
+        <ProductList />
+
+        <ShoppingCart />
+
+      </section>
+      
+      <section className="flex flex-col gap-4">
+        <Counter />
+        <Counter />
+      </section>
     </div>
   )
 }
