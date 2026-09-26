c094ec97d6de104bffd387a4d53acee5fec51f03
Victor Villazón <victorzn23@gmail.com>
2026-09-25 22:08:12 -0500

    semana12, dia02, 03, react zustand counter shopping cart app tailwindcss

--- semana12/dia02-03-react-zustand-counter-shopping-cart-app/src/components/ProductList.jsx
+++ semana12/dia02-03-react-zustand-counter-shopping-cart-app/src/components/ProductList.jsx
@@ -1,6 +1,28 @@
 const ProductList = ({ products }) => {
   return (
-    <div>ProductList</div>
+    <section className="flex flex-col p-4">
+      <h3 className="text-2xl mb-4 text-center">Product list</h3>
+
+      <section className="grid grid-col-1 md:grid-cols-4 gap-4">
+        {products.map(product => {
+          return (
+            <article
+              key={product.id}
+              className="bg-amber-100 p-2 min-w-14 rounded-lg flex flex-col items-center gap-2"
+            >
+              <img src={product.thumbnail} alt={product.title} />
+              <p className="font-bold text-center">{product.title}</p>
+              <p className="font-bold text-center">S/ {product.price}</p>
+              <button
+                className="bg-blue-400 p-2 min-w-36 rounded-lg cursor-pointer text-white font-bold hover:bg-blue-500 duration-300"
+              >
+                Add to cart
+              </button>
+            </article>
+          )
+        })}
+      </section>
+    </section>
   )
 }
 
