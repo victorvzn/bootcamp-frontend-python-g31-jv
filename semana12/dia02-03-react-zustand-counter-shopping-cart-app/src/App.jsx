52472f186ecf30f4c91911c0012fd73c1d050da9
Victor Villazón <victorzn23@gmail.com>
2026-09-25 21:57:28 -0500

    semana12, dia02, 03, react zustand counter shopping cart app tailwindcss

--- semana12/dia02-03-react-zustand-counter-shopping-cart-app/src/App.jsx
+++ semana12/dia02-03-react-zustand-counter-shopping-cart-app/src/App.jsx
@@ -1,17 +1,34 @@
+import { useEffect } from "react"
 import Counter from "./components/Counter"
 
 import ProductList from "./components/ProductList"
 import ShoppingCart from "./components/ShoppingCart"
+import { useState } from "react"
 
 const App = () => {
+  const [products, setProducts] = useState([])
+
+  const fetchProducts = async () => {
+    const response = await fetch('https://dummyjson.com/products')
+
+    return await response.json()
+  }
+
+  useEffect(() => {
+    fetchProducts()
+      .then(data => setProducts(data.products))
+  }, [])
+
   return (
     <div className="flex flex-col gap-8 p-4">
 
       <section className="flex gap-2">
 
-        <ProductList />
+        <ProductList products={products} />
 
         <ShoppingCart />
+
+        {/* <pre>{JSON.stringify(products, null, 2)}</pre> */}
 
       </section>
       
--- semana12/dia02-03-react-zustand-counter-shopping-cart-app/src/components/ProductList.jsx
+++ semana12/dia02-03-react-zustand-counter-shopping-cart-app/src/components/ProductList.jsx
@@ -1,4 +1,4 @@
-const ProductList = () => {
+const ProductList = ({ products }) => {
   return (
     <div>ProductList</div>
   )
